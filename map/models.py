77f27b0f68107d3c149ed37be6a4fad5e1fcbfad
Manqing You <my2692@columbia.edu>
2020-10-16 13:02:03 +0000

    Modified

--- map/models.py
+++ map/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.utils.translation import gettext as _
+
 class sightings(models.Model):
     Latitude=models.FloatField(
+	    null=True,blank=True,
             help_text=_('Latitude'),
     )
     Longitude=models.FloatField(
+	    null=True,blank=True,
             help_text=_('Longitude'),
     )
     Unique_Squirrel_ID=models.CharField(
@@ -18,11 +21,14 @@ class sightings(models.Model):
             (AM,'AM'),
     )
     Shift=models.CharField(
-            max_length=2,
+            max_length=20,
+	    blank=True,
             choices=Shift_choices,
     )
     Date=models.DateField(
             help_text=_('Date'),
+			null=True,
+			blank=True,
     )
     adult='adult'
     juvenile='juvenile'
@@ -35,6 +41,7 @@ class sightings(models.Model):
     Age=models.CharField(
             max_length=10,
             choices=Age_Choices,
+			blank=True,
     )
     gray='gray'
     cinnamon='cinnamon'
@@ -49,7 +56,7 @@ class sightings(models.Model):
     Primary_Fur_Color=models.CharField(
             max_length=20,
             choices=Fur_color_choices,
-            default=other,
+            blank=True,
             help_text=_('Primary Fur Color'),
     )
     ground_plane='ground plane'
@@ -62,27 +69,28 @@ class sightings(models.Model):
     Location=models.CharField(
             max_length=20,
             choices=Location_choices,
-            default=other,
+            blank=True,
     )
     Specific_location=models.CharField(
             max_length=200,
             help_text=_('Specific location'),
+	    blank=True,
     )
-    Running=models.BooleanField(default=False)
-    Chasing=models.BooleanField(default=False)
-    Climbing=models.BooleanField(default=False)
-    Eating=models.BooleanField(default=False)
-    Foraging=models.BooleanField(default=False)
-    Other_Activities=models.CharField(max_length=200)
-    Kuks=models.BooleanField(default=False)
-    Quaas=models.BooleanField(default=False)
-    Moans=models.BooleanField(default=False)
-    Tail_flags=models.BooleanField(default=False)
-    Tail_twitches=models.BooleanField(default=False)
-    Approaches=models.BooleanField(default=False)
-    Indifferent=models.BooleanField(default=False)
-    Runs_from=models.BooleanField(default=False)
+    Running=models.BooleanField(blank=True)
+    Chasing=models.BooleanField(blank=True)
+    Climbing=models.BooleanField(blank=True)
+    Eating=models.BooleanField(blank=True)
+    Foraging=models.BooleanField(blank=True)
+    Other_Activities=models.CharField(max_length=200,blank=True)
+    Kuks=models.BooleanField(blank=True)
+    Quaas=models.BooleanField(blank=True)
+    Moans=models.BooleanField(blank=True)
+    Tail_flags=models.BooleanField(blank=True)
+    Tail_twitches=models.BooleanField(blank=True)
+    Approaches=models.BooleanField(blank=True)
+    Indifferent=models.BooleanField(blank=True)
+    Runs_from=models.BooleanField(blank=True)
     def __str__(self):
         return self.Unique_Squirrel_ID
-
-# Create your models here.
+    def getattrlist():
+        return [i.name for i in sightings._meta.fields]
--- map/views.py
+++ map/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from sightings.models import sightings
 def mymap(request):
     mylist=sightings.objects.all()[:100]
-    context={'mylst':mylist,}
+    context={'mylist':mylist,}
     return render(request,'map/map.html',context)
 
 # Create your views here.

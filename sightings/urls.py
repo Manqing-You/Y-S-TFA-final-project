7003638834fac8026d3a15785351a489d5de2a6a
Manqing You <my2692@columbia.edu>
2020-10-16 06:26:44 +0000

    Modified the views and urls again

--- sightings/urls.py
+++ sightings/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
   
 from . import views
 urlpatterns=[
-        path('sightings/',views.mymap,name='map'),
+        #path('sightings/',views.mymap,name='sightings'),
+        path('map/',views.mymap,name='mymap'),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from sightings.models import sightings
 def mymap(request):
     mylist=sightings.objects.all()[:100]
-    context={'mylst':mylist,}
+    context={'mylist':mylist,}
     return render(request,'sightings/map.html',context)
 # Create your views here.
